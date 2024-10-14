5f7a65f2ef93ce9d0343e75d19204bc79e703067
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-10-14 21:37:04 +0100

    cyclist entity

--- src/main/java/com/ouchin/entity/Cyclist.java
+++ src/main/java/com/ouchin/entity/Cyclist.java
@@ -1,5 +1,40 @@
 package com.ouchin.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+
+import java.time.LocalDate;
+
 @Entity
+@Table(name = "cyclists")
 public class Cyclist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank(message = "The first name  is required")
+    @Column(name = "last_name", nullable = false)
+    private String LastName0;
+
+    @NotBlank(message = "The last name is required")
+    @Column(name = "first_name", nullable = false)
+    private String FirstName;
+
+    @NotBlank(message = "Nationaly is required")
+    @Column(nullable = false)
+    private String nationality;
+
+    @NotBlank(message = "The birthay is required")
+    @Past(message = "The birthay must be in the past")
+    @Column(nullable = false)
+    private LocalDate Birthday;
+
+    @NotBlank(message = "The team is required")
+    @Column(nullable = false)
+    private String team;
+
+
+
 }
