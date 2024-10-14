504fdb6362fb73f255ddf200a8f83ac92a132d1e
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-10-14 21:57:16 +0100

    result entity

---
+++ src/main/java/com/ouchin/entity/Result.java
@@ -0,0 +1,26 @@
+package com.ouchin.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.time.Duration;
+
+@Entity
+@Table(name = "results")
+public class Result {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Time is required")
+    @Column(nullable = false)
+    private Duration time;
+
+
+    @Positive(message = "Rank must be positive")
+    @Column(nullable = false)
+    private Integer rank;
+
+
+}
